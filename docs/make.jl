56fbe587f1f7354dc3131d33e3a663702d87f872
Jérémy Béjanin <jeremy.bejanin@gmail.com>
2020-04-07 12:22:48 -0400

    fix repo url

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, OntarioCOVID19, SparseArrays, PyPlot
 DocMeta.setdocmeta!(OntarioCOVID19, :DocTestSetup, :(using OntarioCOVID19); recursive=true)
 
 makedocs(
-    sitename = "OntarioCOVID19.jl",
+    sitename = "OntarioCOVID19",
     authors = "Jérémy Béjanin.",
     modules = [OntarioCOVID19],
     format = Documenter.HTML(prettyurls = false),
@@ -15,7 +15,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/jebej/OntarioCOVID19.jl.git",
+    repo = "github.com/jebej/OntarioCOVID19.git",
     target = "build",
     deps = nothing,
     make = nothing,
