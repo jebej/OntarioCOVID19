ead0f386ea04845ef0f1142ceb76281e3740bee5
jebej <jebej@users.noreply.github.com>
2021-01-21 18:13:05 +0000

    Deploying to gh-pages from  @ cf91bf200b56672438eb7843a033bb323fe67409 🚀

--- assets/index/code/figsetup.jl
+++ assets/index/code/figsetup.jl
@@ -1,6 +1,6 @@
 # This file was generated, do not modify it. # hide
 #hideall
 using OntarioCOVID19
-df = get_summary_data()
-p1,p2,p3,p4,p5,p6 = plot_all_plotly(df)
+p1,p2,p3,p4,p5,p6 = plot_all_plotly()
+pv1,pv2 = plot_vaccination_data()
 nothing
